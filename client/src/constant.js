126268382f8c9d42d02899ca5affd5f0687c86a7
Anurag[Cuvette TA] <anurag@cuvette.tech>
2024-09-25 10:43:56 +0530

    Update constant.js

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,3 +1,3 @@
-const BACKEND_URL = "https://backend-capstone-4la3.onrender.com";
-
+//const BACKEND_URL = "https://backend-capstone-4la3.onrender.com";
+const BACKEND_URL = "https://backend-capstone-1-9zvx.onrender.com";
 export { BACKEND_URL };
